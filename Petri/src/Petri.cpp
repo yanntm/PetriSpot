e590584708b14f8c9a3bda95c129cd3f49b5cb74
Etienne Renault <renault@lrde.epita.fr>
2020-11-05 14:38:28 +0100

    Petri: setup parsing for a given formula
    * src/Petri.cpp: Here.

--- Petri/src/Petri.cpp
+++ Petri/src/Petri.cpp
@@ -16,12 +16,23 @@
 
 #include "Petricube.h"
 
+// Spot's includes, keep it sorted alphabetically
+#include <spot/tl/apcollect.hh>
+#include <spot/tl/defaultenv.hh>
+#include <spot/tl/parse.hh>
+#include <spot/twaalgos/dot.hh>
+#include <spot/twaalgos/translate.hh>
+#include <spot/twacube/twacube.hh>
+#include <spot/twacube_algos/convert.hh>
+
 using namespace std;
 
 int main(void)
 {
   bool display = true;
   bool ltl = true;
+  std::string formula = "GF a && GF b"; // FIXME
+
   try {
     SparsePetriNet * pn = loadXML("model.pnml");
 
@@ -34,6 +45,35 @@ int main(void)
 
     if (ltl)
       {
+	// Setup the environment and default dictionnary
+	spot::default_environment& env = spot::default_environment::instance();
+	auto dict = spot::make_bdd_dict();
+
+	// Parse formula...
+	auto pf = spot::parse_infix_psl(formula, env, false);
+	auto f = pf.f;
+
+	// and translate it!
+	spot::translator trans(dict);
+	auto prop = trans.run(&f);
+
+	// collect atomic propositions
+	spot::atomic_prop_set aps;
+	atomic_prop_collect(f, &aps);
+
+
+	std::cout << "\nWorking with the following atomic propositions:\n";
+	for (spot::atomic_prop_set::const_iterator ap = aps.begin();
+	     ap != aps.end(); ++ap)
+	  {
+	    std::cout << "   " << ap->ap_name() << '\n';
+	  }
+
+	// Build the equivalent twacube
+	auto propcube = spot::twa_to_twacube(prop);
+
+	// FIXME do something with propcube
+
 	auto* pc = new Petricube();
 	// FIXME do something with petricube
 	delete pc;
