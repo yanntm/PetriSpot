9981729bc6d0b5fdc50e265897a9cb39d442ba56
Etienne Renault <renault@lrde.epita.fr>
2020-11-05 10:25:18 +0100

    PTNetLoader: fix implicit conversion  warning
    * Petri/src/PTNetLoader.cpp: Here.

--- Petri/src/PTNetLoader.cpp
+++ Petri/src/PTNetLoader.cpp
@@ -1,8 +1,8 @@
 /*
  * PTNetLoader.cpp
  *
- *  Created on: Oct 13, 2020
- *      Author: ythierry
+ * Created on: Oct 13, 2020
+ * Author: ythierry
  */
 
 #include "PTNetLoader.h"
@@ -12,30 +12,29 @@
 
 
 SparsePetriNet * loadXML(std::string filename) {
-	char buf[BUFSIZ];
-	XML_Parser parser = XML_ParserCreate(NULL);
-	int done;
-	PTNetHandler handler;
+  char buf[BUFSIZ];
+  XML_Parser parser = XML_ParserCreate(NULL);
+  int done;
+  PTNetHandler handler;
 
-	FILE * in = fopen(filename.c_str(),"r");
+  FILE * in = fopen(filename.c_str(),"r");
 
-	XML_SetUserData(parser, &handler);
-	XML_SetElementHandler(parser, &PTNetHandler::startElement, &PTNetHandler::endElement);
-	XML_SetCharacterDataHandler(parser, &PTNetHandler::characters);
-	do {
-		size_t len = fread(buf, 1, sizeof(buf), in);
-		done = len < sizeof(buf);
-		if (XML_Parse(parser, buf, (int)len, done) == XML_STATUS_ERROR) {
-			fprintf(stderr, "%s at line %d \n",
-					XML_ErrorString(XML_GetErrorCode(parser)),
-					XML_GetCurrentLineNumber(parser));
-			XML_ParserFree(parser);
-			return nullptr;
-		}
-	} while (! done);
+  XML_SetUserData(parser, &handler);
+  XML_SetElementHandler(parser, &PTNetHandler::startElement, &PTNetHandler::endElement);
+  XML_SetCharacterDataHandler(parser, &PTNetHandler::characters);
+  do {
+    size_t len = fread(buf, 1, sizeof(buf), in);
+    done = len < sizeof(buf);
+    if (XML_Parse(parser, buf, (int)len, done) == XML_STATUS_ERROR)
+      {
+	fprintf(stderr, "%s at line %d \n",
+		XML_ErrorString(XML_GetErrorCode(parser)),
+		(int)XML_GetCurrentLineNumber(parser));
 	XML_ParserFree(parser);
-	fclose(in);
-	return handler.getParseResult();
+	return nullptr;
+      }
+  } while (! done);
+  XML_ParserFree(parser);
+  fclose(in);
+  return handler.getParseResult();
 }
-
-
