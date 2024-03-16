cf917dda83b755b44fdaf1e8b900485c4c23ce23
Soufiane El Mouahid <s.elmouahid@hotmail.com>
2024-03-16 05:23:21 +0100

    Update build.sh

--- build_ddd.sh
+++ build_ddd.sh
@@ -1,13 +1,27 @@
-#! /bin/bash
+wget --progress=dot:mega https://ftp.gnu.org/gnu/gmp/gmp-6.3.0.tar.xz ; 
+tar xf gmp-6.3.0.tar.xz ; cd gmp-6.3.0 ; 
+./configure --enable-cxx --enable-fat --prefix=$(pwd)/../usr/local  --build=westmere-pc-linux-gnu ; m
+ake -j ; 
+make install ; 
+cd .. ;
 
-git clone https://github.com/lip6/libDDD.git
+wget --progress=dot:mega https://github.com/libexpat/libexpat/archive/R_2_2_4.tar.gz ; 
+tar xzf R_2_2_4.tar.gz ; 
+cd libexpat-R_2_2_4/expat/ ; 
+./buildconf.sh ; 
+./configure --prefix=$(pwd)/../../usr/local --without-xmlwf ; 
+make -j ; 
+make install ; 
+cd ../.. ;
 
-cd libDDD
+wget --progress=dot:mega https://github.com/lip6/libDDD/raw/gh-pages/linux.tgz ; 
+tar xzf linux.tgz ; 
+rm linux.tgz
 
-./configure --prefix=$PWD/../
+wget --progress=dot:mega https://yanntm.github.io/Spot-BinaryBuilds/spot_linux.tar.gz ; 
+tar xzf spot_linux.tar.gz ;
 
-make -j2 install
-
-
-
-
+cd Petri ; 
+autoreconf -vfi ;
+./configure --prefix=$PWD/../usr/local/  --with-libexpat=$PWD/../usr/local/ --with-libspot=$(pwd)/../usr/local/ --with-libddd=$PWD/../usr/local/ --with-gmp=$PWD/../usr/local/ --with-antlrc=$PWD/../usr/local/   CPPFLAGS="-I$(pwd)/../usr/local/include -DNDEBUG" LDFLAGS="-L$(pwd)/../usr/local/lib" || cat config.log ;
+make -j 4 ;
