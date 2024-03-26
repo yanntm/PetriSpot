2108c6415940edf896fa1f872595f7de657a7c18
Soufiane El Mouahid <s.elmouahid@hotmail.com>
2024-03-26 09:00:29 +0100

    Update Petri.cpp

--- Petri/src/Petri.cpp
+++ Petri/src/Petri.cpp
@@ -134,7 +134,8 @@ int main(int argc, char * argv[]) {
 //					//InvariantCalculator.printInvariant(invarT, strtnames, empty );
 				}
 //				SparseIntArray inv = DeadlockTester.findPositiveTsemiflow(sumMatrix);
-//
+//				
+//				delete pn;
 //				return null;
 			}
 
