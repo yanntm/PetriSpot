63b23288e3a56a4ec355d539c9181135d3d103d1
soufianeelm <s.elmouahid@hotmail.com>
2024-04-02 05:51:33 +0200

    small corrections

--- Petri/src/InvariantMiddle.h
+++ Petri/src/InvariantMiddle.h
@@ -20,7 +20,7 @@ static MatrixCol computeReducedFlow(const SparsePetriNet & sr, std::vector<int> 
         
 		typedef ext_hash_map<const SparseIntArray *, int> map_t;
 		map_t::accessor acc;
-        	map_t seen;
+        map_t seen;
 
 		for (int i=0 ; i < sr.getFlowPT().getColumnCount() ; i++) {
 			SparseIntArray combined = SparseIntArray::sumProd(-1, sr.getFlowPT().getColumn(i), 1, sr.getFlowTP().getColumn(i));
@@ -31,58 +31,11 @@ static MatrixCol computeReducedFlow(const SparsePetriNet & sr, std::vector<int> 
                 		sumMatrix.appendColumn(combined);
 				tnames.push_back(i);
 			}
-            		representative.push_back((*acc).second);
+            		representative.push_back(acc->second);
 		}
-        	// add logger ?
+        // add logger ?
 	}
 	return sumMatrix;
 }
 
-/*
-static void printInvariant(Collection<SparseIntArray> invariants, List<String> pnames, List<Integer> initial,
-		PrintStream out) {
-	for (SparseIntArray rv : invariants) {
-		StringBuilder sb = new StringBuilder();
-		int sum = printEquation(rv, initial, pnames, sb);
-		out.println("inv : " + sb.toString() + " = " + sum);
-	}
-	out.println("Total of " + invariants.size() + " invariants.");
-}
 
-static void printInvariant(Collection<SparseIntArray> invariants, List<String> pnames,
-		List<Integer> initial) {
-	printInvariant(invariants, pnames, initial, System.out);
-}
-
-static int printEquation(SparseIntArray inv, List<Integer> initial, List<String> pnames, StringBuilder sb) {
-	boolean first = true;
-	int sum = 0;
-	for (int i = 0; i < inv.size(); i++) {
-		int k = inv.keyAt(i);
-		int v = inv.valueAt(i);
-		if (v != 0) {
-			if (first) {
-				if (v < 0) {
-					sb.append("-");
-					v = -v;
-				}
-				first = false;
-			} else {
-				if (v < 0) {
-					sb.append(" - ");
-					v = -v;
-				} else {
-					sb.append(" + ");
-				}
-			}
-			if (v != 1) {
-				sb.append(v + "*" + pnames.get(k));
-			} else {
-				sb.append(pnames.get(k));
-			}
-			sum += initial != null ? v * initial.get(k) : 0;
-		}
-	}
-	return sum;
-}
-*/
