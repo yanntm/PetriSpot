944c127ed02ab064b2db9f45d1925a08087639c2
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2024-05-28 17:00:26 +0200

    template parameter for size of values

--- Petri/src/FlowMatrix.h
+++ Petri/src/FlowMatrix.h
@@ -3,65 +3,67 @@
 
 #include "MatrixCol.h"
 
+template<typename T>
 class FlowMatrix {
-	// To represent the flow matrix, if we can build it. We use a sparse representation.
-	// Map variable index -> Transition index -> update to variable (a relative integer)
+    // To represent the flow matrix, if we can build it. We use a sparse representation.
+    // Map variable index -> Transition index -> update to variable (a relative integer)
 private:
-	MatrixCol flow ;
-	MatrixCol read ;
-	MatrixCol flowPT ;
-	MatrixCol flowTP ;
+    MatrixCol<T> flow;
+    MatrixCol<T> read;
+    MatrixCol<T> flowPT;
+    MatrixCol<T> flowTP;
 
 public:
-	FlowMatrix (int nbvar, int nbtrans) : flow(nbvar, nbtrans), read(nbvar, nbtrans), flowPT(nbvar, nbtrans), flowTP(nbvar, nbtrans) {
-	}
-	
-	void addWriteEffect(int tindex, int vindex, int val) {
-		if (val == 0)
-			return;
-		addToColumn(flow.getColumn(tindex), vindex, val);
-		if (val < 0) {
-			addToColumn(flowPT.getColumn(tindex), vindex, -val);
-		} else {
-			addToColumn(flowTP.getColumn(tindex), vindex, val);
-		}
-	}
+    FlowMatrix(int nbvar, int nbtrans)
+        : flow(nbvar, nbtrans), read(nbvar, nbtrans), flowPT(nbvar, nbtrans), flowTP(nbvar, nbtrans) {
+    }
+
+    void addWriteEffect(int tindex, int vindex, T val) {
+        if (val == 0)
+            return;
+        addToColumn(flow.getColumn(tindex), vindex, val);
+        if (val < 0) {
+            addToColumn(flowPT.getColumn(tindex), vindex, -val);
+        } else {
+            addToColumn(flowTP.getColumn(tindex), vindex, val);
+        }
+    }
 
 private:
-	void addToColumn(SparseIntArray column, int vindex, int val) {
-		int cur = column.get(vindex);				
-		cur+=val;
-		column.put(vindex, cur);
-	}
+    void addToColumn(SparseArray<T>& column, int vindex, T val) {
+        T cur = column.get(vindex);
+        cur += val;
+        column.put(vindex, cur);
+    }
 
 public:
-	void addReadEffect(int tindex, int vindex, int val) {
-		SparseIntArray line = flowPT.getColumn(tindex);
-		int cur = line.get(vindex);				
-		int max= std::max(cur,val);
-		if (max != cur) {
-			line.put(vindex, max);
-			addToColumn(flowTP.getColumn(tindex), vindex, max - cur);
-		}
-		read.getColumn(tindex).put(vindex, max);
-	}
-	
-	MatrixCol getIncidenceMatrix() {
-		return flow;
-	}
-	
-	MatrixCol getRead() {
-		return read;
-	}
-	
-	MatrixCol getFlowPT() {
-		return flowPT;
-	}
-	
-	MatrixCol getFlowTP() {
-		return flowTP;
-	}
-	
+    void addReadEffect(int tindex, int vindex, T val) {
+        SparseArray<T>& line = flowPT.getColumn(tindex);
+        T cur = line.get(vindex);
+        T max = std::max(cur, val);
+        if (max != cur) {
+            line.put(vindex, max);
+            addToColumn(flowTP.getColumn(tindex), vindex, max - cur);
+        }
+        read.getColumn(tindex).put(vindex, max);
+    }
+
+    MatrixCol<T> getIncidenceMatrix() const {
+        return flow;
+    }
+
+    MatrixCol<T> getRead() const {
+        return read;
+    }
+
+    MatrixCol<T> getFlowPT() const {
+        return flowPT;
+    }
+
+    MatrixCol<T> getFlowTP() const {
+        return flowTP;
+    }
 };
+
 
 #endif /* FLOWMATRIX_H_ */
