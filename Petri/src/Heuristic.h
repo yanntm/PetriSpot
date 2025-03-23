025230969b3549f4ec9df2c2cb889e2ca8b105e1
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2025-03-23 17:02:07 +0100

    new flag for Qplus basis

--- Petri/src/Heuristic.h
+++ Petri/src/Heuristic.h
@@ -19,13 +19,15 @@ public:
            PivotStrategy pivotStrategy = PivotStrategy::FindBest,
            ssize_t loopLimit = -1,
            bool useCulling = true,
-           bool minimize = false)
+           bool minimize = false,
+           bool useQPlusBasis=false)
         : useSingleSignRow_(useSingleSignRow),
           pivotStrategy_(pivotStrategy),
           loopLimit_((loopLimit == -1) ? std::numeric_limits<size_t>::max()
                                       : static_cast<size_t>(loopLimit)),
                                         useCulling_(useCulling),
-                                        minimize_(minimize)
+                                        minimize_(minimize),
+                                        useQPlusBasis_(useQPlusBasis)
     {}
 
     bool useSingleSignRow() const { return useSingleSignRow_; }
@@ -33,12 +35,14 @@ public:
     size_t getLoopLimit() const { return loopLimit_; }
     bool useCulling() const { return useCulling_; }
     bool useMinimization() const { return minimize_; }
+    bool useQPlusBasis() const { return useQPlusBasis_; }
 private:
     const bool useSingleSignRow_;
     const PivotStrategy pivotStrategy_;
     const size_t loopLimit_;
     const bool useCulling_;
     const bool minimize_;
+    const bool useQPlusBasis_;
 };
 
 } // namespace petri
